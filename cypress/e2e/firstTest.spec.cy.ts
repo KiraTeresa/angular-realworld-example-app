b7ed1f8894e43f3579dd8539b404a18f190df715
KiraTeresa <kira.soba@gmx.de>
2023-05-09 11:06:15 +0200

    Mocking API response

--- cypress/e2e/firstTest.spec.cy.ts
+++ cypress/e2e/firstTest.spec.cy.ts
@@ -1,6 +1,7 @@
 describe('Test with backend', () => {
 
   beforeEach('login to the app', () => {
+    cy.intercept('GET', 'https://api.realworld.io/api/tags', {fixture: 'tags.json'});
     cy.loginToApplication();
   });
 
@@ -28,10 +29,46 @@ describe('Test with backend', () => {
     cy.contains('Publish Article').click();
 
     cy.wait('@postArticles').then(xhr => {
-      console.log(xhr);
       expect(xhr.response.statusCode).to.equal(200);
       expect(xhr.request.body.article.body).to.equal('This is a body of the article');
       expect(xhr.response.body.article.description).to.equal('This is a description');
     });
+  });
+
+  it('verify popular tags are displayed', () => {
+
+    // my solution:
+    /*    cy.get('.tag-list a').then(tag => {
+          cy.wrap(tag).contains('cypress');
+          cy.wrap(tag).contains('automation');
+          cy.wrap(tag).contains('testing');
+        });*/
+
+    // course solution:
+    cy.get('.tag-list')
+      .should('contain', 'cypress')
+      .and('contain', 'automation')
+      .and('contain', 'testing');
+  });
+
+  it.only('verify global feed likes count', () => {
+    cy.intercept('GET', 'https://api.realworld.io/api/articles/feed*', {'articles': [], 'articlesCount': 0});
+    cy.intercept('GET', 'https://api.realworld.io/api/articles*', {fixture: 'articles.json'});
+
+    cy.contains('Global Feed').click();
+
+    cy.get('app-article-list button')
+      .then(heartList => {
+        expect(heartList[0]).to.contain('1');
+        expect(heartList[1]).to.contain('5');
+      });
+
+    cy.fixture('articles').then(file => {
+      const articleLink = file.articles[1].slug;
+      file.articles[1].favoritesCount = 6;
+      cy.intercept('POST', `https://api.realworld.io/api/articles/${articleLink}/favorite`, file);
+    });
+
+    cy.get('app-article-list button').eq(1).click().should('contain', '6');
   });
 });
